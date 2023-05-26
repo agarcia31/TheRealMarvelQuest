3046da52a63046f8eecf1f179e289bc324e77cb3
Andy <a.garcia.atn31@gmail.com>
2023-05-26 02:20:45 -0700

    reworked api keys

--- src/assets/js/ironman.js
+++ src/assets/js/ironman.js
@@ -1,40 +1,69 @@
-let bio = document.getElementById("bio");
+let bio = document.getElementById("ironman_bio");
 let comics = document.getElementById("comics");
 let movies = document.getElementById("movies");
 let ironman_image = document.getElementById("ironman_image");
-let ironman_bio = document.getElementById("ironman_bio");
 let comic_image = document.getElementById("comic_image");
 let comic_title = document.getElementById("comic_title");
-fetch(
-  "https://gateway.marvel.com/v1/public/characters?name=Iron%20Man&ts=HelloWorld123&apikey=58cf9dccb6a433565258dd4185258cc5&hash=6a3f4d8c242a80ba92ccd1d638bc7c4f"
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    //start of thumbnail//
-    console.log(data.data.results[0].thumbnail.path + ".jpg");
-    let thumbnail = data.data.results[0].thumbnail.path + ".jpg";
-    ironman_image.src = thumbnail;
-    //end of thumbnail//
 
-    //start of bio//
-    console.log(data.data.results[0].description);
-    let bio = data.data.results[0].description;
-    ironman_bio.textContent += bio;
-    //end of bio//
+const ts = 'HelloWorld123';
+const apiKey = '58cf9dccb6a433565258dd4185258cc5';
+const hash = '6a3f4d8c242a80ba92ccd1d638bc7c4f';
+const characterName = 'Iron Man';
 
-    //start of comic//
-    console.log(data.data.results[0].comics);
-    let comic = data.data.results[0].comics.items;
+function getIronManData() {
+  const apiUrl = `https://gateway.marvel.com/v1/public/characters?name=${encodeURIComponent(characterName)}&ts=${ts}&apikey=${apiKey}&hash=${hash}`;
 
-    fetch(
-      "https://gateway.marvel.com/v1/public/comics/30090?ts=HelloWorld123&apikey=58cf9dccb6a433565258dd4185258cc5&hash=6a3f4d8c242a80ba92ccd1d638bc7c4f"
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        comic_image.innerHTML = `<img src=${response.data.results[0].images[0].path}.${response.data.results[0].images[0].extension}>`;
-        console.log(response.data);
-      })
-      .catch((err) => console.error(err));
-  });
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // ...
+
+      // Fetch and render comics
+      data.data.results[0].comics.items.forEach(comic => {
+        const comicId = comic.resourceURI.split('/').pop();
+        const comicApiUrl = `https://gateway.marvel.com/v1/public/comics/${comicId}?ts=${ts}&apikey=${apiKey}&hash=${hash}`;
+
+        fetch(comicApiUrl)
+          .then(response => response.json())
+          .then(comicData => {
+            const comic = comicData.data.results[0];
+
+            // Render comic title
+            let comicTitle = comic.title;
+            let comicImage = comic.thumbnail.path + '.' + comic.thumbnail.extension;
+
+            // Create comic element
+            let comicElement = document.createElement('div');
+            comicElement.classList.add('mb-2');
+
+            // Create comic title element
+            let titleElement = document.createElement('h3');
+            titleElement.classList.add('text-lg', 'font-bold');
+            titleElement.textContent = comicTitle;
+            comicElement.appendChild(titleElement);
+
+            // Create comic image element
+            let imageElement = document.createElement('img');
+            imageElement.src = comicImage;
+            imageElement.alt = comicTitle;
+            imageElement.classList.add('w-full', 'max-w-xs', 'mb-2');
+            comicElement.appendChild(imageElement);
+
+            // Append comic element to the comics container
+            comics.appendChild(comicElement);
+          })
+          .catch(error => {
+            console.log('Error fetching comic:', error);
+          });
+      });
+    })
+    .catch(error => {
+      console.log('Error fetching Iron Man data:', error);
+    });
+}
+
+getIronManData();
+
+
+
+getIronManData();
