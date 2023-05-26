b58ddb6cb9fa9c85e2adee326da201a439a999de
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2023-05-26 05:01:19 -0700

    Fix banner image. Add search bar and button

---
+++ src/assets/js/search.js
@@ -0,0 +1,20 @@
+
+
+                                    // Possible search function for the search bar 
+
+
+
+
+<script>
+    function search() {
+        // Get the value entered in the search bar
+        var searchTerm = document.getElementById("searchBar").value;
+
+        // Perform the search action
+        // You can use the searchTerm variable to search for superheroes and display the results
+
+        // For now, let's just log the search term in the console
+        console.log("Search term:", searchTerm);
+    }
+</script>
+
